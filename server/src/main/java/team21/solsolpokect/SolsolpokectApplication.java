4ddebc6d48fe8457006ce2fcf951c5837810151c
kdozlo <eodud5730@naver.com>
2023-09-03 14:08:36 +0900

    Init: 초기 서버 코드

---
+++ server/src/main/java/team21/solsolpokect/SolsolpokectApplication.java
@@ -0,0 +1,13 @@
+package team21.solsolpokect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolsolpokectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolsolpokectApplication.class, args);
+	}
+
+}
