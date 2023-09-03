51cd35e95786be176a8507cf56b2d35f37b49606
kdozlo <eodud5730@naver.com>
2023-09-03 18:13:40 +0900

    Feat: BaseTime 객체 생성\nclose #5

---
+++ server/src/main/java/team21/solsolpokect/common/entity/BaseTime.java
@@ -0,0 +1,28 @@
+package team21.solsolpokect.common.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTime {
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    protected LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    protected LocalDateTime updateAt;
+
+    @Column(nullable = true)
+    private LocalDateTime deletedDate;
+}
