6cdd03795a03b53a6bba56c1efa1ab90ebe04d28
kdozlo <eodud5730@naver.com>
2023-09-03 23:40:59 +0900

    Feat: Mission Entity 생성

--- server/src/main/java/team21/solsolpokect/mission/entity/Mission.java
+++ server/src/main/java/team21/solsolpokect/mission/entity/Mission.java
@@ -1,4 +1,51 @@
 package team21.solsolpokect.mission.entity;
 
-public class Mission {
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import team21.solsolpokect.common.entity.BaseTime;
+import team21.solsolpokect.user.entity.Users;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Mission extends BaseTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String missionName;
+
+    @Column(nullable = false)
+    private int reward;
+
+    @Column(nullable = false)
+    private boolean complete;
+
+    @Column(nullable = false)
+    private String goal;
+
+    private String picture;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID", nullable = false)
+    private Users user;
+
+    private boolean allow;
+
+    @Builder
+    public Mission(String missionName, int reward, boolean complete, String goal, String picture, Users user, boolean allow) {
+        this.missionName = missionName;
+        this.reward = reward;
+        this.complete = complete;
+        this.goal = goal;
+        this.picture = picture;
+        this.user = user;
+        this.allow = allow;
+    }
 }
