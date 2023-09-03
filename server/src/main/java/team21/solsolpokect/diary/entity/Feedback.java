dd169a554c16c33602765ba059a39f5316ee2c0e
Seyoung Oh <osy9536@naver.com>
2023-09-03 21:53:09 +0900

    Feat : feedback 엔티티 초안 작성

--- server/src/main/java/team21/solsolpokect/diary/entity/Feedback.java
+++ server/src/main/java/team21/solsolpokect/diary/entity/Feedback.java
@@ -1,4 +1,25 @@
 package team21.solsolpokect.diary.entity;
 
-public class Feedback {
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import team21.solsolpokect.common.entity.BaseTime;
+import team21.solsolpokect.user.entity.Users;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Feedback extends BaseTime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String contents;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private Users users;
 }
