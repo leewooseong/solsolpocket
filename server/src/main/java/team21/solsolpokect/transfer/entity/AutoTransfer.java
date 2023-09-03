6142aa455245d8997d3b028ae594b3a07b816dcd
kdozlo <eodud5730@naver.com>
2023-09-03 23:45:33 +0900

    Feat: AutoTransfer Entity 생성

--- server/src/main/java/team21/solsolpokect/transfer/entity/AutoTransfer.java
+++ server/src/main/java/team21/solsolpokect/transfer/entity/AutoTransfer.java
@@ -1,4 +1,42 @@
 package team21.solsolpokect.transfer.entity;
 
-public class AutoTransfer {
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import team21.solsolpokect.common.entity.BaseTime;
+import team21.solsolpokect.user.entity.Users;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class AutoTransfer extends BaseTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private LocalDateTime autoDate;
+
+    @Column(nullable = false)
+    private int money;
+
+    @Column(nullable = false)
+    private int childAccount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID", nullable = false)
+    private Users user;
+
+    @Builder
+    public AutoTransfer(LocalDateTime autoDate, int money, int childAccount, Users user) {
+        this.autoDate = autoDate;
+        this.money = money;
+        this.childAccount = childAccount;
+        this.user = user;
+    }
 }
