364089e029bf0e780ca7dfec98b0b78c1bc88579
Seyoung Oh <osy9536@naver.com>
2023-09-05 23:08:52 +0900

    Feat : Diary 패키지 ResponseDto 작성 및 불필요 dto 삭제

--- server/src/main/java/team21/solsolpokect/diary/dto/response/diary/DiaryInfoDetailResponseDto.java
+++ server/src/main/java/team21/solsolpokect/diary/dto/response/diary/DiaryInfoDetailResponseDto.java
@@ -1,4 +1,17 @@
 package team21.solsolpokect.diary.dto.response.diary;
 
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Getter
 public class DiaryInfoDetailResponseDto {
+    private LocalDate dealDate;
+    private LocalTime dealTime;
+    private int withdrawMoney;
+    private int depositMoney;
+    private String dealContents;
+    private int balance;
+    private String storeName;
 }
--- server/src/main/java/team21/solsolpokect/diary/dto/response/feedback/FeedbackInfosResponseDto.java
+++ server/src/main/java/team21/solsolpokect/diary/dto/response/feedback/FeedbackInfosResponseDto.java
@@ -1,4 +1,13 @@
 package team21.solsolpokect.diary.dto.response.feedback;
 
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
 public class FeedbackInfosResponseDto {
+    private Long feedbackId;
+    private String contents;
+    private LocalDateTime createAt;
+    private LocalDateTime deletedAt;
 }
