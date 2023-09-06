22e7145b26c1122dd4a71db4aef8344d6308aa9d
Seyoung Oh <osy9536@naver.com>
2023-09-06 22:21:04 +0900

    Feat : 가족 구성원 가계부 상세 조회 API 작성
    가족 구성원 가계부 상세 조회 API를 작성하기 위해 controller, service 작성 및 DiaryInfoDetailResponseDto의 정적 팩토리 메서드 작성
    
    close #23

--- server/src/main/java/team21/solsolpokect/diary/controller/DiaryController.java
+++ server/src/main/java/team21/solsolpokect/diary/controller/DiaryController.java
@@ -1,4 +1,29 @@
 package team21.solsolpokect.diary.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import team21.solsolpokect.common.response.ApiResponseDto;
+import team21.solsolpokect.common.response.MsgType;
+import team21.solsolpokect.common.response.ResponseUtils;
+import team21.solsolpokect.diary.dto.response.diary.DiaryInfoDetailResponseDto;
+import team21.solsolpokect.diary.service.DiaryService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/diary")
+@RequiredArgsConstructor
 public class DiaryController {
+    private final DiaryService diaryService;
+
+    @GetMapping("/family-info/detail")
+    public ApiResponseDto<List<DiaryInfoDetailResponseDto>> diaryCheck(@RequestParam Long userId,
+                                                                      @RequestParam("diary-date") String diaryDate) throws JsonProcessingException {
+
+        return ResponseUtils.ok(diaryService.diaryCheck(userId, diaryDate), MsgType.DIARY_CHECK_SUCCESSFULLY);
+    }
 }
