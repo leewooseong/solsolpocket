b2d8d82947ac146fef846c6cfc79c57af9cc0572
kdozlo <eodud5730@naver.com>
2023-09-06 21:45:37 +0900

    Feat : #16 Mission 패키지 DTO 작성 close #16

--- server/src/main/java/team21/solsolpokect/mission/dto/request/MissionAllowRequestDto.java
+++ server/src/main/java/team21/solsolpokect/mission/dto/request/MissionAllowRequestDto.java
@@ -1,4 +1,10 @@
 package team21.solsolpokect.mission.dto.request;
 
+import lombok.Getter;
+
+@Getter
 public class MissionAllowRequestDto {
+
+    private long userId;
+    private boolean allow;
 }
--- server/src/main/java/team21/solsolpokect/mission/dto/request/MissionPictureRequestDto.java
+++ server/src/main/java/team21/solsolpokect/mission/dto/request/MissionPictureRequestDto.java
@@ -1,4 +1,10 @@
 package team21.solsolpokect.mission.dto.request;
 
+import lombok.Getter;
+
+@Getter
 public class MissionPictureRequestDto {
+
+    private long userId;
+    private String picture;
 }
