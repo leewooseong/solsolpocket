fa58b82b866d8bd4ae2f0a8c7102f50fd5448827
leewooseong <sitpo96@naver.com>
2023-09-07 09:53:25 +0900

    Feat: GoBackHeader 컴포넌트 추가

---
+++ client/components/GoBackHeader.jsx
@@ -0,0 +1,34 @@
+import { COLORS, SIZES, FONTS, icons, images } from '../constants';
+
+const GoBackHeader = ({ title }) => {
+  return (
+    <TouchableOpacity
+      style={{
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: SIZES.padding * 6,
+        paddingHorizontal: SIZES.padding * 2,
+      }}>
+      <Image
+        source={icons.back}
+        resizeMode="contain"
+        style={{
+          width: 20,
+          height: 20,
+          tintColor: 'black',
+        }}
+      />
+
+      <Text
+        style={{
+          marginLeft: SIZES.padding * 1.5,
+          color: 'black',
+          ...FONTS.h4,
+        }}>
+        {title}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+export default GoBackHeader;
