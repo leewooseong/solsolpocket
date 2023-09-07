4867c911b721004ba79efad0efff3d0a62d63642
kdozlo <eodud5730@naver.com>
2023-09-07 20:20:12 +0900

    Feat : #18 Transfer 패키지 DTO 작성 close #18

--- server/src/main/java/team21/solsolpokect/transfer/dto/request/AutoTransferModifyRequestDto.java
+++ server/src/main/java/team21/solsolpokect/transfer/dto/request/AutoTransferModifyRequestDto.java
@@ -1,4 +1,12 @@
 package team21.solsolpokect.transfer.dto.request;
 
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
 public class AutoTransferModifyRequestDto {
+
+    private LocalDateTime autoDate;
+    private int money;
 }
