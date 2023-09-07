525e10610065e9819b3831047aad8474eb3f6545
dalcheonroadhead <wjsaos2081@gmail.com>
2023-09-08 01:44:24 +0900

    Feat: 사진 인증 과제 화면 작성 및 카메라 모달 연결

--- client/pages/QuestType2.js
+++ client/pages/QuestType2.js
@@ -1,13 +1,155 @@
-<<<<<<< HEAD
-import { View, Text } from "react-native";
-=======
-import { View, Text } from 'react-native';
->>>>>>> 65c18af6f778e069f5045379bd314cef3ac929ed
+/* eslint-disable prettier/prettier */
+import { useState } from 'react';
+import {
+  FlatList,
+  SafeAreaView,
+  Text,
+  View,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  Appearance,
+  Button,
+} from 'react-native';
+import { Entypo } from '@expo/vector-icons';
+import { DateTimePickerModal } from 'react-native-modal-datetime-picker';
+import { useQuestList } from '../hooks/use-questList';
+import useCamera from '../hooks/use-camera';
+import CameraModal from '../components/CameraModal';
+import TakeOrShowPic from '../components/TakeOrShowPic';
 
-export default () => {
+export default ({ navigation }) => {
+  const DATA = [
+    {
+      id: 1,
+      title: '목표 기한',
+      content: 'text value',
+    },
+    {
+      id: 2,
+      title: '도전내용',
+      content: 'text value',
+    },
+
+    {
+      id: 3,
+      title: '보상 내용',
+      content: 'text value',
+    },
+    {
+      id: 4,
+      title: '도전자',
+      content: 'text value',
+    },
+    {
+      id: 5,
+      title: 'NPC가 남긴 말',
+      content: 'text value',
+    },
+  ];
+
+  const {
+    image,
+    cameraRef,
+    modalVisible,
+    setModalVisible,
+    toggle,
+    takePicture,
+    photoAlready,
+    onPressToggle,
+    onPressCamera,
+  } = useCamera();
+
+  const renderItem = ({ item: { title, content } }) => {
+    return (
+      <View style={styles.forView}>
+        <Text style={styles.forText}>{title}</Text>
+        <Text style={styles.forText}>{content}</Text>
+      </View>
+    );
+  };
+
   return (
-    <View>
-      <Text>사진 인증 유형입니다.</Text>
+    <View style={styles.forFullScreen}>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+
+      <TakeOrShowPic
+        onPress={onPressCamera}
+        photoAlready={photoAlready}
+        image={image}
+        navigation={navigation}
+      />
+
+      <CameraModal
+        cameraRef={cameraRef}
+        modalVisible={modalVisible}
+        setModalVisible={setModalVisible}
+        onPressToggle={onPressToggle}
+        type={toggle}
+        takePicture={takePicture}
+      />
+
+      <TouchableOpacity
+        style={styles.forLastButton}
+        onPress={() => {
+          navigation.navigate(`Main`);
+        }}>
+        <Text style={styles.forLastText}> 제출하기</Text>
+      </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  forFullScreen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  forView: {
+    flex: 3,
+    paddingTop: 20,
+    paddingBottom: 20,
+  },
+  forText: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    marginLeft: 13,
+    fontStyle: 'italic',
+  },
+
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderTopWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderBottomColor: '#3D70FF',
+    borderRadius: 20,
+  },
+  buttonContainer: {
+    backgroundColor: '#3D70FF',
+  },
+  forLastButton: {
+    flex: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#3D70FF',
+    alignSelf: 'center',
+    borderRadius: 50,
+    width: 200,
+    marginBottom: 50,
+  },
+  forLastText: {
+    color: '#ffffff',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
