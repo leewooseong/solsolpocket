f1fce069ecd67669a35839bef80d53f4c6371de8
Seyoung Oh <osy9536@naver.com>
2023-09-07 21:06:07 +0900

    Chore : 피드백 request Dto 통합

--- server/src/main/java/team21/solsolpokect/diary/dto/request/feedback/FeedbackRequestDto.java
+++ server/src/main/java/team21/solsolpokect/diary/dto/request/feedback/FeedbackRequestDto.java
@@ -3,7 +3,7 @@ package team21.solsolpokect.diary.dto.request.feedback;
 import lombok.Getter;
 
 @Getter
-public class FeedbackCreateRequestDto {
+public class FeedbackRequestDto {
 
     private String userId;
     private String contents;
