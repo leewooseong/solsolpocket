95d413f2c9b1e33a188e3215106e4c3ba19d6b72
dalcheonroadhead <wjsaos2081@gmail.com>
2023-09-08 01:42:18 +0900

    Feat: 머니 챌린지 화면 작성

--- client/pages/QuestType1.js
+++ client/pages/QuestType1.js
@@ -1,13 +1,121 @@
-<<<<<<< HEAD
-import { View, Text } from "react-native";
-=======
-import { View, Text } from 'react-native';
->>>>>>> 65c18af6f778e069f5045379bd314cef3ac929ed
+/* eslint-disable prettier/prettier */
+import { useState } from 'react';
+import {
+  FlatList,
+  SafeAreaView,
+  Text,
+  View,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  Appearance,
+  Button,
+} from 'react-native';
 
-export default () => {
+import { DateTimePickerModal } from 'react-native-modal-datetime-picker';
+import { useQuestList } from '../hooks/use-questList';
+
+export default ({ navigation }) => {
+  const DATA = [
+    {
+      id: 1,
+      title: '목표 기한',
+      content: 'text value',
+    },
+    {
+      id: 2,
+      title: '도전 내용',
+      content: 'text value',
+    },
+
+    {
+      id: 3,
+      title: '보상이 여러분을 기다려요...',
+      content: 'text value',
+    },
+    {
+      id: 4,
+      title: '도전자',
+      content: 'text value',
+    },
+    {
+      id: 5,
+      title: '퀘스트 NPC가 남긴 말...',
+      content: 'text value',
+    },
+  ];
+
+  const renderItem = ({ item: { title, content } }) => {
+    return (
+      <View style={styles.forView}>
+        <Text style={styles.forText}>{title}</Text>
+        <Text style={styles.forText}>{content}</Text>
+      </View>
+    );
+  };
+
   return (
-    <View>
-      <Text>머니챌린지 유형입니다.</Text>
+    <View style={styles.forFullScreen}>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+      <TouchableOpacity
+        style={styles.forLastButton}
+        onPress={() => {
+          navigation.navigate(`Main`);
+        }}>
+        <Text style={styles.forLastText}> 홈으로 돌아가기</Text>
+      </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  forFullScreen: {
+    flex: 1,
+  },
+
+  forView: {
+    flex: 3,
+    paddingTop: 20,
+    paddingBottom: 20,
+  },
+  forText: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    marginLeft: 13,
+    fontStyle: 'italic',
+  },
+
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderTopWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderBottomColor: '#3D70FF',
+    borderRadius: 20,
+  },
+  buttonContainer: {
+    backgroundColor: '#3D70FF',
+  },
+  forLastButton: {
+    flex: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#3D70FF',
+    alignSelf: 'center',
+    borderRadius: 50,
+    width: 200,
+    marginBottom: 50,
+  },
+  forLastText: {
+    color: '#ffffff',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
