cf0827067e9e59e02c19f2a70e83bcf9e20e2f65
Seyoung Oh <osy9536@naver.com>
2023-09-10 16:54:51 +0900

    Feat : FeedbackController 작성

--- server/src/main/java/team21/solsolpokect/diary/controller/FeedbackController.java
+++ server/src/main/java/team21/solsolpokect/diary/controller/FeedbackController.java
@@ -1,4 +1,24 @@
 package team21.solsolpokect.diary.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import team21.solsolpokect.common.response.ApiResponseDto;
+import team21.solsolpokect.common.response.MsgType;
+import team21.solsolpokect.common.response.ResponseUtils;
+import team21.solsolpokect.diary.dto.request.feedback.FeedbackRequestDto;
+import team21.solsolpokect.diary.service.FeedbackService;
+
+@RestController
+@RequestMapping("api/diary/feedback")
+@RequiredArgsConstructor
 public class FeedbackController {
+
+    private final FeedbackService feedbackService;
+
+    @PostMapping("/create")
+    public ApiResponseDto<Void> feedbackCreate(@RequestBody FeedbackRequestDto requestDto) {
+
+        feedbackService.feedbackCreate(requestDto);
+        return ResponseUtils.ok(MsgType.DIARY_CREATE_SUCCESSFULLY);
+    }
 }
