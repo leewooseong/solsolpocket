d71e7d4af207893f547fc4315e495514b67aef54
Seyoung Oh <osy9536@naver.com>
2023-09-10 16:55:10 +0900

    Feat : FeedbackService 초안작성

--- server/src/main/java/team21/solsolpokect/diary/service/FeedbackService.java
+++ server/src/main/java/team21/solsolpokect/diary/service/FeedbackService.java
@@ -1,4 +1,18 @@
 package team21.solsolpokect.diary.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import team21.solsolpokect.diary.dto.request.feedback.FeedbackRequestDto;
+import team21.solsolpokect.diary.repository.FeedbackRepository;
+
+@Service
+@RequiredArgsConstructor
 public class FeedbackService {
+
+    private final FeedbackRepository repository;
+
+    public void feedbackCreate(FeedbackRequestDto requestDto) {
+        String contents = requestDto.getContents();
+
+    }
 }
