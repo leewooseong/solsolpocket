9b808038ad47da066d2c6d6b8ad200abd28df49c
leewooseong <sitpo96@naver.com>
2023-09-10 16:13:53 +0900

    Feat: AccountBook 컴포넌트를 위한 DummyData 추가

---
+++ client/test/dummyData/user.js
@@ -0,0 +1,42 @@
+export const dummyUser = {
+  name: '대영',
+  role: '부모',
+  account: 111,
+  credit_score: 200,
+  userId: 'b',
+};
+
+export const dummyUsers = [
+  {
+    name: '세영',
+    role: '부모',
+    account: 222,
+    credit_score: 200,
+    userId: 'a',
+  },
+  {
+    name: '대영',
+    role: '부모',
+    account: 111,
+    credit_score: 200,
+    userId: 'b',
+  },
+  {
+    name: '우성',
+    role: '자녀',
+    account: 333,
+    credit_score: 200,
+    userId: 'c',
+  },
+  {
+    name: '수민',
+    role: '자녀',
+    account: 444,
+    credit_score: 200,
+    userId: 'd',
+  },
+];
+
+export const dummyFamily = {
+  familyName: '대영이네',
+};
