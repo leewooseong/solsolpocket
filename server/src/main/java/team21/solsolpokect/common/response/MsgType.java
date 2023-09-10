b7e00399e7f348cd2ed528cbaacdfaa9cd256f6d
Seyoung Oh <osy9536@naver.com>
2023-09-10 16:50:41 +0900

    Feat : MsgType 추가 및 수정

--- server/src/main/java/team21/solsolpokect/common/response/MsgType.java
+++ server/src/main/java/team21/solsolpokect/common/response/MsgType.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 public enum MsgType {
 
     DIARY_CHECK_SUCCESSFULLY("가계부 조회 성공"),
+    DIARY_CREATE_SUCCESSFULLY("피드백 작성 성공"),
+    GOAL_MONEY_CREATE_SUCCESSFULLY("월별 목표 금액 설정 성공"),
     ;
 
     private final String msg;
