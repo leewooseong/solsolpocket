4c9f2180c85d0049e5d61a990a9822657760c9ba
Seyoung Oh <osy9536@naver.com>
2023-09-10 16:59:07 +0900

    Feat : feedback MsgType 추가

--- server/src/main/java/team21/solsolpokect/common/response/MsgType.java
+++ server/src/main/java/team21/solsolpokect/common/response/MsgType.java
@@ -8,6 +8,7 @@ public enum MsgType {
     DIARY_CHECK_SUCCESSFULLY("가계부 조회 성공"),
     DIARY_CREATE_SUCCESSFULLY("피드백 작성 성공"),
     GOAL_MONEY_CREATE_SUCCESSFULLY("월별 목표 금액 설정 성공"),
+    FEEDBACK_CREATE_SUCCESSFULLY("피드백 작성 성공"),
     ;
 
     private final String msg;
